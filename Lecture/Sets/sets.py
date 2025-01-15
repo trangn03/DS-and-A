4b2626021925c56397fd4d6b92fd998e3110d44a
Trang Ngo <xtrang0201@gmail.com>
2025-01-14 23:22:32 -0800

    Create sets.py

---
+++ Lecture/Sets/sets.py
@@ -0,0 +1,15 @@
+# Example 1: Creating a set with duplicate elements
+unique_number = {1, 2, 3, 4, 5}
+print("Unique numbers: ", unique_number)
+
+# Example 2: Using set() constructor
+programming_languages = set(["Python", "Java", "C++"])
+print("Programming languages: ", programming_languages)
+
+# Example 3: Creating an empty set
+empty_set = set()
+print("Empty set: ", empty_set)
+
+# Example 4: Creating an imuutable set
+immutable_set = frozenset([10, 20, 30])
+print("Immutable set: ", immutable_set)
