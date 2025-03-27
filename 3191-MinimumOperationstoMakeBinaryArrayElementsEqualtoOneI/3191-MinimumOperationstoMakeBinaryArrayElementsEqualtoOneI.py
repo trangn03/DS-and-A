ab0170958fb92f6e68dad96558bb0dcbb56dcab9
trangn03 <89541716+trangn03@users.noreply.github.com>
2025-03-26 21:30:00 -0700

    File updated at 3/26/2025, 9:30:01 PM

--- 3191-MinimumOperationstoMakeBinaryArrayElementsEqualtoOneI/3191-MinimumOperationstoMakeBinaryArrayElementsEqualtoOneI.py
+++ 3191-MinimumOperationstoMakeBinaryArrayElementsEqualtoOneI/3191-MinimumOperationstoMakeBinaryArrayElementsEqualtoOneI.py
@@ -1,11 +1,15 @@
+# Last updated: 3/26/2025, 9:30:01 PM
 class Solution:
-    def divideArray(self, nums: List[int]) -> bool:
-        # Array, Hash table
-        # Initialize a counter collection
-        counter = collections.Counter(nums)
-        # Loop through the array
-        # Mod (even - odd)
-        for value in counter.values():
-            if value % 2 == 1:
-                return False 
-        return True        
+    def minOperations(self, nums: List[int]) -> int:
+        count = 0
+        n = len(nums)
+        for i in range(n):
+            if nums[i] == 0:
+                count += 1
+                if i + 1 >= n or i + 2 >= n:
+                    return -1
+                nums[i] ^= 1
+                nums[i + 1] ^= 1
+                nums[i + 2] ^= 1
+        return count 
+        
