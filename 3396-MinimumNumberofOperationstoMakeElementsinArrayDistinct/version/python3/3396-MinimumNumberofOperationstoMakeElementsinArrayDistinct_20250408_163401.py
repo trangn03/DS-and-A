5d198d2cc0382a8ed811c72405cb45681e99c3ae
trangn03 <89541716+trangn03@users.noreply.github.com>
2025-04-08 16:34:01 -0700

    Create file

---
+++ 3396-MinimumNumberofOperationstoMakeElementsinArrayDistinct/version/python3/3396-MinimumNumberofOperationstoMakeElementsinArrayDistinct_20250408_163401.py
@@ -0,0 +1,9 @@
+# Last updated: 4/8/2025, 4:34:01 PM
+class Solution:
+    def minimumOperations(self, nums: List[int]) -> int:
+        seen = set()
+        for i in range(len(nums) - 1, -1, -1):
+            if nums[i] in seen:
+                return i // 3 + 1
+            seen.add(nums[i])
+        return 0
